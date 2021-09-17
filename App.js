8d280f5413b8bd5c7322c511522f5101b7d550c4
Rogerio Reis <rogerio.rtr46@gmail.com>
2021-09-17 10:22:23 -0300

    useEffect funcionando, trasendo o dado salvo anteriormente

--- App.js
+++ App.js
@@ -1,9 +1,17 @@
-import React, {useState} from 'react';
-import {Button, Keyboard, StyleSheet, TextInput, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {
+  Button,
+  Keyboard,
+  StyleSheet,
+  TextInput,
+  View, Text,
+  YellowBox,
+} from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function App() {
   const [nome, setNome] = useState('');
+  const [nomeSalvo, setNomeSalvo] = useState('');
 
   async function handleGravarNome() {
     await AsyncStorage.setItem('nome', nome);
@@ -11,19 +19,35 @@ export default function App() {
     Keyboard.dismiss();
   }
 
+  // Similar ao ComponentDidMount e ComponentDiUpdate
+  // Carregar uma única vez antres de renderizar todos os componentes
+  useEffect(async () => {
+
+    // Buscando o dado que foi salvo anteriormente
+    await AsyncStorage.getItem('nome').then(value => {
+      setNomeSalvo(value);
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
-      <TextInput value={nome} onChangeText={setNome} />
+      <TextInput value={nome} onChangeText={setNome} style={styles.input} />
       <Button title="Salvar Nome" onPress={handleGravarNome}></Button>
+      <View>
+        <TextInput style={styles.input}>{nomeSalvo}</TextInput>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    // flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    backgroundColor: 'yellow',
+    width: '100%',
   },
 });
